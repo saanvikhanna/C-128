1f40596e36e7cd659a8e56ec58cf2094adeaf032
saanvikhanna <87762768+saanvikhanna@users.noreply.github.com>
2023-04-03 14:42:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+song_1 = "";
+song_2 = "";
+
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function preload()
+{
+    song_1 = loadSound("music.mp3");
+    song_2 = loadSound("music2.mp3");
+}
+
+function play()
+{
+    song_1.play();
+    song_1.setVolume(1);
+    song_1.rate(1);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet model is Initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("left Wrist x = " + leftWristX + ",left wrist y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("right Wrist x = " + rightWristX + ",right wrist y = " + rightWristY)
+    }
+}
